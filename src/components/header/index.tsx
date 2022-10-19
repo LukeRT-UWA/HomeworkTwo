08b2f8b967013e9f6404f8885fd991386804afce
LukeRT-UWA <16175583curtin@gmail.com>
2022-10-19 18:52:48 +0800

    initial header layout

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,12 +2,23 @@ import React from "react";
 import style from './index.module.css'
 
 export type Props = {
-    children?: React.ReactNode
+  children?: React.ReactNode
 }
 
-export const Header: React.FC<Props> = ({children}) => {
-    return (
-        <div className={style.test}>{children}</div>
-    )
+export const Header: React.FC<Props> = ({ children }) => {
+  return (
+    <div className={style.headerWrapper}>
+      <div className={style.headerLayout}>
+        <div>Logo</div>
+        <div className={style.menuItems}> 
+          <div>Option 1</div>
+          <div>Option 2</div>
+          <div>Option 3</div>
+          <div>Option 4</div>
+        </div>
+      </div>
+      <div className={style.belowBorder}></div>
+    </div>
+  )
 
 }
