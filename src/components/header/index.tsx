9017b2ba0760bf0b2c4473bb4ebdeb556fc222ca
LukeRT-UWA <16175583curtin@gmail.com>
2022-10-22 17:04:28 +0800

    refactor transition with pure css, need to implement correct transition

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,44 +1,52 @@
-import React, { useEffect, useRef } from "react";
-import style from './index.module.css';
-import sr from "../../utils/sr";
-import { srHeaderConfig } from "../../utils/srConfig";
+import React, { useEffect, useRef, useState } from "react";
+import styles from './index.module.css';
 
 export type Props = {
   children?: React.ReactNode
 }
 
-export const Header: React.FC<Props> = ({ children }) => {
-  const logoRef = useRef(null)
-  const menuItem1 = useRef(null)
-  const menuItem2 = useRef(null)
-  const menuItem3 = useRef(null)
-  const menuItem4 = useRef(null)
+export const Header: React.FC<Props> = ({ }) => {
+
   useEffect(() => {
-    if (logoRef.current !== null &&
-      menuItem1.current !== null && 
-      menuItem2.current !== null && 
-      menuItem3.current !== null && 
-      menuItem4.current !== null) {
-   sr?.reveal(logoRef.current, srHeaderConfig(100))     
-   sr?.reveal(menuItem1.current, srHeaderConfig(200))
-   sr?.reveal(menuItem2.current, srHeaderConfig(300))
-   sr?.reveal(menuItem3.current, srHeaderConfig(400))
-   sr?.reveal(menuItem4.current, srHeaderConfig(500))
-  }
-  },[]);
+    setInProp(!inProp)
+  }, []);
+
+  const nodeRef = useRef(null);
+  const [inProp, setInProp] = useState(false);
   return (
-    <div className={style.headerWrapper}>
-      <div className={style.headerLayout}>
-        <div ref={logoRef}>Logo</div>
-        <div className={style.menuItems}> 
-          <div ref={menuItem1}>Option 1</div>
-          <div ref={menuItem2}>Option 2</div>
-          <div ref={menuItem3}>Option 3</div>
-          <div ref={menuItem4}>Option 4</div>
-        </div>
+    <div className={styles.headerWrapper}>
+      <div className={styles.headerLayout}>
+        <div >Logo</div>
+        <div className={styles.menuItems}>
+          <div 
+          className={inProp ? styles.myThingDone : styles.myThing}  
+          style={{transitionDelay: `200ms`}}
+          >
+            Test !!!
+            </div>
+            <div 
+          className={inProp ? styles.myThingDone : styles.myThing}  
+          style={{transitionDelay: `400ms`}}
+          >
+            Test !!!
+            </div>
+            <div 
+          className={inProp ? styles.myThingDone : styles.myThing}  
+          style={{transitionDelay: `600ms`}}
+          >
+            Test !!!
+            </div>
+            <div 
+          className={inProp ? styles.myThingDone : styles.myThing}  
+          style={{transitionDelay: `800ms`}}
+          >
+            Test !!!
+            </div>
+          </div>
       </div>
-      <div className={style.belowBorder}></div>
+      <div className={styles.belowBorder}></div>
     </div>
+
   )
 
 }
